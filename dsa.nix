2316bd63d3468438013d494987ae32ae9d989576
Emery Hemingway <emery@iowacitydsa.org>
2017-05-12 10:17:26 -0500

    Initial commit

---
+++ dsa.nix
@@ -0,0 +1,39 @@
+{ config, lib, ... }:
+
+let
+  acmeCallbackPort = 80;
+  adminMail = "tech@iowacitydsa.org";
+in
+
+{
+  services.mlmmj =
+    { enable = true;
+      listDomain = "iowacitydsa.org";
+      mailLists = [ "exec" "list" ];
+    };
+
+  services.postfix =
+    { extraAliases = builtins.readFile ./aliases;
+      # Private email aliases are stored in this file,
+      # included an alias for $adminMail
+
+      destination = [ "iowacitydsa.org" ];
+      domain = "iowacitydsa.org";
+      enableSubmission = true;
+      origin = "mail.iowacitydsa.org";
+      postmasterAlias = adminMail;
+      relayDomains = [ "iowacitydsa.org" ];
+      sslCACert = "/var/lib/acme/mail.iowacitydsa.org/account_key.json";
+      sslKey = "/var/lib/acme/mail.iowacitydsa.org/key.pem";
+      submissionOptions.smtpd_hard_error_limit = "2";
+    };
+
+  services.nginx.virtualHosts."mail.iowacitydsa.org" =
+    { enableACME = true;
+      port = acmeCallbackPort;
+    };
+
+  security.acme.certs."mail.iowacitydsa.org".email = adminMail;
+
+  networking.firewall.allowedTCPPorts = [ 25 587 acmeCallbackPort ];
+}
